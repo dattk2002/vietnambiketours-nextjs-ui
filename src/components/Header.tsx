4ef0d9633b36d988ea582ee24fcb0d8b229fb084
biahudaom <kimdat0705@gmail.com>
2024-05-27 16:50:23 +0700

    fix: update router

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,14 +6,18 @@ import React from "react";
 const Header = () => {
   return (
     <div className="container pl-80">
-      <Image
-        src="/images/logo_vnbt.jpg" // Path to your image
-        alt="Logo"
-        width={238}
-        height={80}
-      />
+      <figure>
+        <Image
+          src="/images/logo_vnbt.jpg" // Path to your image
+          alt="Logo"
+          width={238}
+          height={80}
+        />
+        <figcaption>An elephant at sunset</figcaption>
+      </figure>
+
       <nav className="flex space-x-4 text-black">
-        <Link href="/">Home</Link>
+        <Link href="/home">Home</Link>
         <Link href="/vietnam">Vietnam</Link>
         <Link href="/cambodia">Cambodia</Link>
         <Link href="/thailand">Thailand</Link>
