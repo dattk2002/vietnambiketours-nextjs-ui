16cc581f068edc67c970fd27a8cad819d10515a1
FanaLusis <duydeptrai01655@gmail.com>
2024-05-28 15:48:49 +0700

    chore: implement text and implement icon rating, brand image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,46 +6,24 @@ import React, { use } from "react";
 import VietnamImg from "@/components/img-country/VietnamImg";
 import { useRouter, usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-// tạo 1 hàm Header
 const Header = () => {
-  // const [isMounted, setIsMounted] = useState(false);
-
-  // const router = useRouter();
-
-  // useEffect(() => {
-  //   setIsMounted(true);
-  // }, []);
-  // if (!isMounted) {
-  //   return null; // Hoặc một loading spinner, hoặc một nội dung dự phòng khác
-  // }
-  // // Kiểm tra router trước khi sử dụng
-  // if (!router || !router.asPath) {
-  //   console.error("Router is not available");
-  //   return <div>Error: Router is not available</div>; // Hoặc xử lý lỗi khác tùy ý bạn
-  // }
-  // // redirect("/vietnam");
-  // console.log(router.pathname);
-  // const country = router.asPath.replace("/", "").split("/");
-  // console.log(country);
-
-  //  v2
   const [isMounted, setIsMounted] = useState(false);
   const pathname = usePathname();
-  const country = pathname ? pathname.replace("/", "").split("/") : []; // Add null check for path
-  //
-  // Handle the case where path is null
+  const country = pathname ? pathname.replace("/", "").split("/") : []; 
+  
   console.log(">>rest", pathname);
   useEffect(() => {
     setIsMounted(true);
   }, []);
   if (!isMounted) {
-    return null; // Hoặc một loading spinner, hoặc một nội dung dự phòng khác
+    return null; 
   }
   console.log(">>pathname", pathname === "/vietnam");
   return (
-    <div className="container pl-80 pr-80">
+    <div className="px-16">
+      <div className="container mx-auto px-72">
       <Image
-        src="/images/logo_vnbt.jpg" // Path to your image
+        src="/images/logo_vnbt.jpg" 
         alt="Logo"
         width={238}
         height={80}
@@ -56,7 +34,7 @@ const Header = () => {
       ></div>
       <VietnamImg country={Array.isArray(country) ? country[0] : ""} />
       <nav className="flex space-x-4 text-black">
-        <Link className={`link ${pathname === "/" ? "active" : ""}`} href="/">
+        <Link className={`link ${pathname === "/" ? "active" : ""}`} href="/home">
           Home
         </Link>
         <Link
@@ -103,10 +81,9 @@ const Header = () => {
         </Link>
       </nav>
       </div>
-     
       
-     
-    </div>
+      </div>
+
   );
 };
 
