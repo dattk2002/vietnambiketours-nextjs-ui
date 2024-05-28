cbaa4424f54a2d073432d97f46a2f58b9ed272a2
FanaLusis <duydeptrai01655@gmail.com>
2024-05-28 18:18:10 +0700

    fix: fix layout home, vietnam, thailand,cambodia

--- src/pages/multi-country.tsx
+++ src/pages/multi-country.tsx
@@ -4,13 +4,49 @@ import React from "react";
 const MultiCountry = () => {
   return (
     <>
-      <div className="flex space-x-4 p-4 ">
-        <Link href="/multi-country/from-vietnam">Cycling from Vietnam</Link>
-        <Link href="/multi-country/from-cambodia">Cycling from Cambodia</Link>
-        <Link href="/multi-country/from-thailand">Cycling from Thailand</Link>
-        <Link href="https://www.facebook.com/MyanmarBicycleTours">
+      <div className="px-16 flex-col grid gap-4 sm:grid-cols-12 pt-5">
+        <nav className="flex-col flex col-span-3 text-[13px]">
+          <Link className="pb-2" href="/alone-traveler">
+          Cycling from Vietnam
+          </Link>
+          <Link className="pb-2" href="group-bike-tours">
+          Cycling from Cambodia
+          </Link>
+          <Link className="pb-2" href="/tailor-make-quote">
+          Cycling from Thailand
+          </Link>
+          <Link className="pb-2" href="/travel-inquiry">
           Cycling Myanmar
-        </Link>
+          </Link>
+          
+          
+        </nav>
+
+        <div className="col-span-9">
+          <p className="text-[13px] pb-3">
+          Home &gt; Multi-country
+          </p>
+          <p className="text-[13px] ">
+          MULTI-COUNTRY IN ONE TRIP BY BIKE
+
+          </p>
+          <p className="text-[13px] pb-3">
+          The countries of Southeast Asia are greatly diverse. With improved travel links, multi-country cycling is a popular way to explore the ancient and modern wonders of the region. On our multi-country tours, you’ll explore historic temples, relax on spectacular beaches, experience buzzing cities, and cycle among the best biking tour guides deep into the SE Asian countryside.
+          </p>
+          <p className="text-[13px] ">
+          We genuinely believe that cycling through a region for the first time with a group of like-minded people is the best way to experience Asia. Our aim is to provide you with the opportunity to meet new people, learn about new cultures, experience different landscapes and, most importantly, to have some fun. 
+          </p>
+          
+          <p className="text-[13px]">
+          Cycling is a major part of our trips – they are cycle tours after all. All of our tours accommodate the preferences of the participating cyclists. Different trips will require varying degrees of physical ability. Specific details on terrain, cycling conditions, distances covered each day and altitude will be discussed prior to your biking adventure.
+          </p>
+          <p className="text-[13px] pb-3">
+          Vietnam s number of visitors for tourism and vacation has increased steadily over 
+the past ten years. About 5 million international guests visited Vietnam last year. The government and private enterprises are investing capital into the coastal regions that are already popular for their beaches and boat tours. Besides those regular tours, Vietnam is perhaps one of the best countries for cycling vacations. It gives travelers more chances to see the countryside and experience local life, compared with other methods of travel.
+Popular destinations include: Hanoi, Saigon, Phong Nha, Hue (the old imperial capital), World Heritage Sites like Hoi An and the Cham temples of Mỹ Sơn, the coastal city of Nha Trang, the caves of Ha Long Bay, the Marble Mountains, and the Mekong Delta.
+
+          </p>
+        </div>
       </div>
     </>
   );
--- src/pages/vietnam.tsx
+++ src/pages/vietnam.tsx
@@ -4,15 +4,79 @@ import React from "react";
 const Vietnam = () => {
   return (
     <>
-      <div className="flex space-x-4 p-4 ">
-        <Link href="/vietnam/from-hcm">Cycling from HCM city</Link>
-        <Link href="/vietnam/mekong-delta">Cycling Mekong Delta</Link>
-        <Link href="/vietnam/from-dalat">Cycling from Da Lat</Link>
-        <Link href="/vietnam/from-nhatrang">Cycling from Nha Trang</Link>
-        <Link href="/vietnam/from-hoian">Cycling from Hoi An</Link>
-        <Link href="/vietnam/from-hue">Cycling from Hue</Link>
-        <Link href="/vietnam/from-sapa">Cycling from Sapa</Link>
-        <Link href="/vietnam/from-hanoi">Cycling from Ha Noi</Link>
+      <div className="px-16 flex-col grid gap-4 sm:grid-cols-12 pt-5">
+        <nav className="flex-col flex col-span-3 text-[13px]">
+          <Link className="pb-2" href="/alone-traveler">
+          Cycling from HCM city
+          </Link>
+          <Link className="pb-2" href="group-bike-tours">
+          Cycling Mekong delta
+          </Link>
+          <Link className="pb-2" href="/tailor-make-quote">
+          Cycling from Da Lat
+          </Link>
+          <Link className="pb-2" href="/travel-inquiry">
+          Cycling from Nha Trang
+          </Link>
+          <Link className="pb-2" href="/visa">
+          Cycling from Hoi An
+          </Link>
+          <Link
+            className="pb-2"
+            href='/'
+          >
+          Cycling from Sapa
+          </Link>
+          <Link
+            className="pb-2"
+            href='/'
+          >
+          Cycling from Hue
+          </Link>
+          <Link
+            className="pb-2"
+            href='/'
+          >
+           Cycling from Ha Noi
+          </Link>
+          
+        </nav>
+
+        <div className="col-span-9">
+          <p className="text-[13px] pb-3">
+            Home &gt; Vietnam &gt; Vietnam Overview
+          </p>
+          <p className="text-[13px] ">
+          VIETNAM OVERVIEW
+          </p>
+          <p className="text-[13px] pb-3">
+          The natural landscape of Vietnam ranges from the fertile, green paddy fields of the Mekong Delta in the south, to long stretches of sandy beaches along the coastal area, with highlands and mountains in the north. A warm and friendly people will welcome you as you explore quiet villages and bustling cities and experience a unique blend of rich heritage and modern tourist facilities. 
+          </p>
+          <p className="text-[13px] ">
+          About Vietnam:
+          </p>
+          <ul className="text-fs pl-10">
+            <li className="list-disc">Official name: Socialist Republic of Vietnam</li>
+            <li className="list-disc">Population: 95,554,759 (April 6, 2018)</li>
+            <li className="list-disc">Capital city: Hanoi (7,6 million); Ho Chi Minh City (8,6 million)</li>
+            <li className="list-disc">Area: 332,698 km2</li>
+            <li className="list-disc">Administrative units: Vietnam is divided into 64 provinces and cities. </li>
+            <li className="list-disc">Weather: Sub-tropical climate with four seasons (Spring, Summer, Autumn, Winter) in the North and two seasons (dry and rainy) in the south.</li>
+            <li className="list-disc">Time zone: GMT +7</li>
+            <li className="list-disc">Religion: Buddhism, Christianity, Catholicism, Islam, Hoa Hao, Cao Dai</li>
+            <li className="list-disc">Currency: The Vietnamese Dong (VND) is the official currency. </li>
+            <li className="list-disc">Video introduction: Click here</li>
+          </ul>
+          <p className="text-[13px]">
+          Cycling in Vietnam:
+          </p>
+          <p className="text-[13px] pb-3">
+          Vietnams number of visitors for tourism and vacation has increased steadily over 
+the past ten years. About 5 million international guests visited Vietnam last year. The government and private enterprises are investing capital into the coastal regions that are already popular for their beaches and boat tours. Besides those regular tours, Vietnam is perhaps one of the best countries for cycling vacations. It gives travelers more chances to see the countryside and experience local life, compared with other methods of travel.
+Popular destinations include: Hanoi, Saigon, Phong Nha, Hue (the old imperial capital), World Heritage Sites like Hoi An and the Cham temples of Mỹ Sơn, the coastal city of Nha Trang, the caves of Ha Long Bay, the Marble Mountains, and the Mekong Delta.
+
+          </p>
+        </div>
       </div>
     </>
   );
